aa344db2ccbf909a4a7104280731b11f0bf682d1
Shawn <shawnduncanard@gmail.com>
2019-03-31 12:38:17 -0500

    fixed sql query

--- node/index.js
+++ node/index.js
@@ -16,31 +16,26 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
     if(err) return err;
+    
 });
-
-
 
 app.use(cors());
 
-
-
 app.get('/', (req, res ) => {
     res.send('go to /products')
-})
+});
 
 app.get('/products/add', (req, res) => {
-    const { prod_name, prod_price} = req.query
-    console.log(prod_name, prod_price);
-    const insertQuery = `insert into products (prod_name, prod_price) values (${prod_name}, ${prod_price});`
+    const { name, price} = req.query
+    const insertQuery = `INSERT INTO products (prod_name, prod_price) values ('${name}', '${price}')`;
     connection.query(insertQuery, (err, results) => {
         if(err) {
             return res.send(err);
         } else {
             return res.send('successful')
         }
-    })
-    res.send('adding product');
-})
+    });
+});
 
 app.get('/products', (req, res) => {
     connection.query(selectAllQuery, (err, results) => {
